0eb3d7684bfd9adc1f12f9eb950fca53e1711788
Rus <de.mr44@gmail.com>
2021-10-11 21:19:40 +0500

    Initial Commit

---
+++ MVC-Storyboard-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-Storyboard-2
+//
+//  Created by ruslan on 11.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
