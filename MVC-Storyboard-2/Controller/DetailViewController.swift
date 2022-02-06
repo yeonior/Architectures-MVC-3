c4e80572919996c856f088f1d155fb51a73f7352
Rus <de.mr44@gmail.com>
2022-02-06 17:45:06 +0500

    Added DetailViewController class

---
+++ MVC-Storyboard-2/Controller/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  MVC-Storyboard-2
+//
+//  Created by Ruslan on 06.02.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var detailImageView: UIImageView! {
+        didSet {
+            if let image = menu?.imageName {
+                detailImageView.image = UIImage(named: image)
+            }
+        }
+    }
+    @IBOutlet weak var detailLabel: UILabel! {
+        didSet {
+            detailLabel.text = menu?.labelName
+        }
+    }
+    
+    var menu: Menu?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
